4f0dfa8f5b928cec1a83b35cb217fcb785239b87
Nayren <rchouchane@iut.univ-paris8.fr>
2025-05-08 16:00:44 +0200

    premiere requetes Ok

--- Polymathee-SApp/src/app/most-common-specialties/most-common-specialties.component.ts
+++ Polymathee-SApp/src/app/most-common-specialties/most-common-specialties.component.ts
@@ -4,18 +4,50 @@ import { CommonModule } from '@angular/common'; // Importez CommonModule si vous
 import { TableModule } from 'primeng/table';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
-
+import { ChartModule } from 'primeng/chart';
+import { ChartData, ChartOptions } from 'chart.js'; // Import ChartData pour la structure des données
 @Component({
   selector: 'app-most-common-specialties',
   standalone: true,
-  imports: [CommonModule, TableModule, IconFieldModule, InputIconModule],
+  imports: [CommonModule, TableModule, IconFieldModule, InputIconModule, ChartModule],
   templateUrl: './most-common-specialties.component.html',
   styleUrl: './most-common-specialties.component.css'
 })
 export class MostCommonSpecialtiesComponent implements OnInit {
   specialties: any[] = [];
   errorMessage: string = '';
+
   loading = true;
+  // Données pour le graphique Doughnut
+  data: ChartData = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [],
+      }
+    ]
+  };
+
+  // Options du graphique
+  options: ChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        labels: {
+          color: '#ffffff'  // Couleur blanche
+        }
+      },
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            return `${tooltipItem.label}: ${tooltipItem.raw}`;
+          }
+        }
+      }
+    }
+  };
 
   constructor(private apiService: ApiService) { }
 
@@ -27,13 +59,32 @@ export class MostCommonSpecialtiesComponent implements OnInit {
     this.apiService.getMostCommonSpecialties().subscribe({
       next: (data) => {
         this.specialties = data;
+        this.updateChartData();
         console.log('Données reçues :', data);
         this.loading = false;
       },
       error: (error) => {
         this.errorMessage = 'Erreur lors de la récupération des données.';
         console.error('Erreur API :', error);
+        this.loading = false;
       }
     });
+  }
+
+  /**
+   * Mise à jour des données du graphique
+   */
+  updateChartData(): void {
+    // Préparer les labels et les données pour le graphique
+    const labels = this.specialties.map(specialtie => specialtie.libelle_specialite);
+    const dataValues = this.specialties.map(specialtie => specialtie.nombre_formations);
+
+    this.data = {
+      labels: labels,
+      datasets: [{
+        data: dataValues,
+        backgroundColor: ['#42A5F5', '#66BB6A', '#FF7043', '#FFEB3B', '#8E24AA']
+      }]
+    };
   }
 }
